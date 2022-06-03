8495e729dfdc37b7735714fa013459cd27055811
pranav_ganesh <e0661424@u.nus.edu>
2022-06-03 10:06:32 +0800

    Add responsive navbar

--- frontend/src/Components/pages/Home.js
+++ frontend/src/Components/pages/Home.js
@@ -1,8 +1,30 @@
+import React, { useRef } from 'react'
+import "../styles/home.css";
 
 export function Home() {
-    return (
-        <div>
-            <h1>This is the home page</h1>
-        </div>
-    )
+  const navBarLinks = useRef(null);
+  const toggleNavbar = () => {
+    // navBarLinks.current.classList.toggle('active')
+    if (navBarLinks.current.style.display === "flex") {
+      navBarLinks.current.style.display = "none"
+    } else {
+      navBarLinks.current.style.display = "flex"
+    }
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="brand-name">Interact</div>
+      <a href="#" className="toggle-button" onClick={toggleNavbar}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+      <div className="nav-links" ref={navBarLinks}>
+        <a href="#">How Interact Works</a>
+        <a href="#">Register</a>
+        <a href="#">Login</a>
+      </div>
+    </nav>
+  );
 }
